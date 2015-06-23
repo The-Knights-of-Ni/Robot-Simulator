93c184567779a2f339a423b73132153ad359cf80
kylerNat <kyler@Kylers-MacBook-Pro.local>
2015-06-23 00:15:36 -0700

    setup osx stuff

--- code/build.sh
+++ code/build.sh
@@ -1,8 +1,8 @@
 mkdir "../osx_build"
 pushd "../osx_build"
 
-rm "simulator.exe"
-clang++ -Wall -O0 -o "simulator" -D DEBUG "../code/sdl_main.cpp" -I "../libraries/SDL2-2.0.3/include" -L "../libraries/SDL2-2.0.3/i686-w64-mingw32/lib" -lmingw32 -lSDL2main -lSDL2
+rm "simulator"
+clang++ -Wall -O0 -o "simulator" -D DEBUG "../code/sdl_main.cpp" -I "../libraries/SDL2-2.0.3/include" -F "./" -framework SDL2 -rpath "@loader_path/"
 
 "./simulator"
 popd
