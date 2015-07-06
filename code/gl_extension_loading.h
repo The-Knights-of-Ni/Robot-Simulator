bdddcf3cfea207a96cd8c520084916e5f7cbf8db
kylerNat <kylerNat@gmail.com>
2015-07-06 01:31:44 -0700

    pulling some stuff

---
+++ code/gl_extension_loading.h
@@ -0,0 +1,130 @@
+#ifndef GL_EXTENSION_LOADING
+#define GL_EXTENSION_LOADING
+
+#include <SDL_opengl.h>
+
+#ifdef _WIN32
+#include "gl/wglext.h"
+
+//opengl functions that require an HDC to get a pointer to
+PFNGLDEBUGMESSAGECALLBACKARBPROC glDebugMessageCallbackARB = 0;
+
+PFNWGLCHOOSEPIXELFORMATARBPROC wglChoosePixelFormatARB = 0;
+PFNWGLCREATECONTEXTATTRIBSARBPROC wglCreateContextAttribsARB = 0;
+
+PFNGLGENBUFFERSPROC glGenBuffers = 0;
+PFNGLBUFFERDATAPROC glBufferData = 0;
+PFNGLBINDBUFFERPROC glBindBuffer = 0;
+PFNGLVERTEXATTRIBPOINTERPROC glVertexAttribPointer = 0;
+PFNGLENABLEVERTEXATTRIBARRAYPROC glEnableVertexAttribArray = 0;
+PFNGLGENVERTEXARRAYSPROC glGenVertexArrays = 0;
+PFNGLBINDVERTEXARRAYPROC glBindVertexArray = 0;
+
+PFNGLCREATESHADERPROC glCreateShader = 0;
+PFNGLSHADERSOURCEPROC glShaderSource = 0;
+PFNGLCOMPILESHADERPROC glCompileShader = 0;
+PFNGLGETSHADERIVPROC glGetShaderiv = 0;
+PFNGLGETSHADERINFOLOGPROC glGetShaderInfoLog = 0;
+PFNGLCREATEPROGRAMPROC glCreateProgram = 0;
+PFNGLATTACHSHADERPROC glAttachShader = 0;
+PFNGLLINKPROGRAMPROC glLinkProgram = 0;
+PFNGLGETPROGRAMIVPROC glGetProgramiv = 0;
+PFNGLGETPROGRAMINFOLOGPROC glGetProgramInfoLog = 0;
+PFNGLDETACHSHADERPROC glDetachShader = 0;
+PFNGLDELETESHADERPROC glDeleteShader = 0;
+PFNGLUSEPROGRAMPROC glUseProgram = 0;
+
+PFNGLGENRENDERBUFFERSPROC glGenRenderbuffers = 0;
+PFNGLBINDRENDERBUFFERPROC glBindRenderbuffer = 0;
+PFNGLRENDERBUFFERSTORAGEPROC glRenderbufferStorage = 0;
+PFNGLGENFRAMEBUFFERSPROC glGenFramebuffers = 0;
+PFNGLBINDFRAMEBUFFERPROC glBindFramebuffer = 0;
+PFNGLFRAMEBUFFERTEXTUREPROC glFramebufferTexture = 0;
+PFNGLFRAMEBUFFERRENDERBUFFERPROC glFramebufferRenderbuffer = 0;
+PFNGLCHECKFRAMEBUFFERSTATUSPROC glCheckFramebufferStatus = 0;
+
+PFNGLACTIVETEXTUREPROC glActiveTexture = 0;
+
+PFNGLGETUNIFORMLOCATIONPROC glGetUniformLocation = 0;
+PFNGLUNIFORM1IPROC glUniform1i = 0;
+PFNGLUNIFORMMATRIX4FVPROC glUniformMatrix4fv = 0;
+
+void loadGLFunctions()
+{        
+    glDebugMessageCallbackARB = (PFNGLDEBUGMESSAGECALLBACKARBPROC) wglGetProcAddress("glDebugMessageCallbackARB");
+        
+    glGenBuffers = (PFNGLGENBUFFERSPROC) wglGetProcAddress("glGenBuffers");
+    glBufferData = (PFNGLBUFFERDATAPROC) wglGetProcAddress("glBufferData");
+    glBindBuffer = (PFNGLBINDBUFFERPROC) wglGetProcAddress("glBindBuffer");
+    glVertexAttribPointer = (PFNGLVERTEXATTRIBPOINTERPROC) wglGetProcAddress("glVertexAttribPointer");
+    glEnableVertexAttribArray = (PFNGLENABLEVERTEXATTRIBARRAYPROC) wglGetProcAddress("glEnableVertexAttribArray");
+    glGenVertexArrays = (PFNGLGENVERTEXARRAYSPROC) wglGetProcAddress("glGenVertexArrays");
+    glBindVertexArray = (PFNGLBINDVERTEXARRAYPROC) wglGetProcAddress("glBindVertexArray");
+        
+    glCreateShader = (PFNGLCREATESHADERPROC) wglGetProcAddress("glCreateShader");
+    glShaderSource = (PFNGLSHADERSOURCEPROC) wglGetProcAddress("glShaderSource");
+    glCompileShader = (PFNGLCOMPILESHADERPROC) wglGetProcAddress("glCompileShader");
+    glGetShaderiv = (PFNGLGETSHADERIVPROC) wglGetProcAddress("glGetShaderiv");
+    glGetShaderInfoLog = (PFNGLGETSHADERINFOLOGPROC) wglGetProcAddress("glGetShaderInfoLog");
+    glCreateProgram = (PFNGLCREATEPROGRAMPROC) wglGetProcAddress("glCreateProgram");
+    glAttachShader = (PFNGLATTACHSHADERPROC) wglGetProcAddress("glAttachShader");
+    glLinkProgram = (PFNGLLINKPROGRAMPROC) wglGetProcAddress("glLinkProgram");
+    glGetProgramiv = (PFNGLGETPROGRAMIVPROC) wglGetProcAddress("glGetProgramiv");
+    glGetProgramInfoLog = (PFNGLGETPROGRAMINFOLOGPROC) wglGetProcAddress("glGetProgramInfoLog");
+    glDetachShader = (PFNGLDETACHSHADERPROC) wglGetProcAddress("glDetachShader");
+    glDeleteShader = (PFNGLDELETESHADERPROC) wglGetProcAddress("glDeleteShader");
+    glUseProgram = (PFNGLUSEPROGRAMPROC) wglGetProcAddress("glUseProgram");
+
+    glGenRenderbuffers = (PFNGLGENRENDERBUFFERSPROC) wglGetProcAddress("glGenRenderbuffers");
+    glBindRenderbuffer = (PFNGLBINDRENDERBUFFERPROC) wglGetProcAddress("glBindRenderbuffer");
+    glRenderbufferStorage = (PFNGLRENDERBUFFERSTORAGEPROC) wglGetProcAddress("glRenderbufferStorage");
+    glGenFramebuffers = (PFNGLGENFRAMEBUFFERSPROC) wglGetProcAddress("glGenFramebuffers");
+    glBindFramebuffer = (PFNGLBINDFRAMEBUFFERPROC) wglGetProcAddress("glBindFramebuffer");
+    glFramebufferTexture = (PFNGLFRAMEBUFFERTEXTUREPROC) wglGetProcAddress("glFramebufferTexture");
+    glFramebufferRenderbuffer = (PFNGLFRAMEBUFFERRENDERBUFFERPROC) wglGetProcAddress("glFramebufferRenderbuffer");
+    glCheckFramebufferStatus = (PFNGLCHECKFRAMEBUFFERSTATUSPROC) wglGetProcAddress("glCheckFramebufferStatus");
+
+    glActiveTexture = (PFNGLACTIVETEXTUREPROC) wglGetProcAddress("glActiveTexture");
+
+    glGetUniformLocation = (PFNGLGETUNIFORMLOCATIONPROC) wglGetProcAddress("glGetUniformLocation");
+    glUniform1i = (PFNGLUNIFORM1IPROC) wglGetProcAddress("glUniform1i");
+    glUniformMatrix4fv = (PFNGLUNIFORMMATRIX4FVPROC) wglGetProcAddress("glUniformMatrix4fv");
+    
+    //TODO: I should probably actualy check these and give a message that the function could not be loaded
+    assert(glDebugMessageCallbackARB);
+        
+    assert(glGenBuffers != 0);
+    assert(glBufferData != 0);
+    assert(glBindBuffer != 0);
+    assert(glVertexAttribPointer);
+    assert(glEnableVertexAttribArray);
+    assert(glGenVertexArrays);
+    assert(glBindVertexArray);
+        
+    assert(glCreateShader != 0);
+    assert(glShaderSource != 0);
+    assert(glCompileShader);
+    assert(glAttachShader);
+    assert(glLinkProgram);
+    assert(glGetProgramiv);
+    assert(glGetProgramInfoLog);
+    assert(glDetachShader);
+    assert(glUseProgram);
+
+    assert(glGenRenderbuffers);
+    assert(glBindRenderbuffer);
+    assert(glRenderbufferStorage);
+    assert(glGenFramebuffers);
+    assert(glBindFramebuffer);
+    assert(glFramebufferTexture);
+    assert(glFramebufferRenderbuffer);
+    assert(glCheckFramebufferStatus);
+
+    assert(glActiveTexture);
+
+    assert(glGetUniformLocation);
+    assert(glUniform1i);
+    assert(glUniformMatrix4fv);
+}
+#endif
+#endif
---
+++ resources/shader.vert
@@ -0,0 +1,10 @@
+#version 440
+
+layout (location = 0) in vec2 p;
+
+void main()
+{
+    gl_Position.xy = p;
+    gl_Position.z = 0.0;
+    gl_Position.w = 1.0;
+}
